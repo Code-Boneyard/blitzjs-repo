6ab82839f15f415bdadfa89e7f5beb96cac6bb48
Max Geller <mxgeller@gmail.com>
2021-12-10 12:45:09 -0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+    fontFamily: {
+      sans: ["Heebo", "sans-serif"],
+    },
   },
   plugins: [],
 }
